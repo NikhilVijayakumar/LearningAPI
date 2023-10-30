54d7a69a0882b8085847bf8dd3e5285c0571a3d0
NikhilVijayakumar <nikhil.vijaykumar@gmail.com>
2023-10-30 12:53:40 +0530

    using waitress

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 # app.py
 
 from main import app, db
+from waitress import serve
 
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run()
+    serve(app, host="0.0.0.0", port=5000)
